b325fe34e84b7d5d820c00f36d283af26e1bd263
teich <oren@teich.net>
2009-02-12 18:38:07 -0800

    Smoothing function now taking average over it's chunk

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -5,7 +5,7 @@ class ChartsController < ApplicationController
     @workout = current_user.workouts.find(params[:workout_id])
     hr_series = @workout.trackpoints.map {|a|a.heart_rate}
 
-    res = smooth_data(hr_series, 15)
+    res = smooth_data(hr_series, 25)    
 
     graph = Scruffy::Graph.new(:theme => Scruffy::Themes::Mephisto.new)
     graph.add(:line, 'Heart Rate', res)
@@ -16,7 +16,29 @@ class ChartsController < ApplicationController
 
     def smooth_data(series, factor)
       res = []
-      (0...series.length).step(factor) {|x| res << series[x]}
+      splits = series.length / factor
+      logger.debug "Averaging #{series.length} trackpoints down to #{splits}"
+      
+      splits.times do |x|
+        first = x * factor
+        last = (x + 1) * factor
+        
+        # logger.debug "Array indicies: #{first} - #{last}"
+        
+        sum = 0
+        datapoints = 0
+        snipit = series[first..last]
+        next if snipit.nil?
+
+        snipit.length.times do |i|
+          next if snipit[i].nil?
+          sum += snipit[i]
+          datapoints += 1
+        end
+        avg = sum.to_f / datapoints
+        res << avg
+      end
       res
     end
+
 end
