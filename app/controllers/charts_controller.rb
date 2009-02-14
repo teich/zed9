cfe0f7710f927552c5b91f0248a6c5b5dc42f4a6
Oren Teich <teich@BigShinyMac.local>
2009-02-13 18:34:30 -0800

    Moved axis in to same stupid loop.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -24,12 +24,15 @@ class ChartsController < ApplicationController
     hr1 = []
     hr2 = []
     hr3 = []
+    axis = []
+    
     graph_data.length.times do |x|
       hr1 << 100
       hr2 << 150
       hr3 << 180
+      axis << x
     end
-    chart.add :axis_category_text, %w[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25] 
+    chart.add :axis_category_text, axis 
     chart.add :series, "", hr1
     chart.add :series, "", hr2
     chart.add :series, "", hr3
