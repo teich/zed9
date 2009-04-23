75c17bbbd9ad443b0238da094544bc2daaa0b4b4
Oren Teich <oren@teich.net>
2009-04-22 23:24:08 -0700

    Removed unnecessary routes

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -10,7 +10,7 @@ class InvitationsController < ApplicationController
       if logged_in?
         Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
         flash[:notice] = "Thank you, invitation sent."
-        redirect_to root_url
+        redirect_to dashboard_url
       else
         flash[:notice] = "Thank you, we will notify when we are ready."
         redirect_to root_url
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :invitations
 
-  map.resource  :account, :controller => "users"
+  map.resource  :account, :controller => "users", :only => [:show, :update, :destroy] 
   map.resources :workouts do |workout|
       #workout.resources :trackpoints
       workout.resource  :chart, :only => [:show]
@@ -12,14 +12,10 @@ ActionController::Routing::Routes.draw do |map|
   map.signup '/signup/:invitation_token', :controller => 'users', :action => 'new'
   map.login '/login', :controller => 'user_sessions', :action => 'new' 
    
+  map.resource  :dashboard, :only => [:show] 
   map.dashboard_chart '/dashboard/chart_data', :controller => 'charts', :action => 'dashboard_duration_data' 
-  map.resource  :dashboard 
+
   map.resource  :user_session
   map.root      :controller => "root", :action => "index"
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-#  map.connect ':controller/:action/:id'
-#  map.connect ':controller/:action/:id.:format'
 end
