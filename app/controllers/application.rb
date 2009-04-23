8a25eaaae8da1f2a005a1a191dd09b80c8e7ca3c
Oren Teich <oren@teich.net>
2009-04-22 22:06:54 -0700

    Added include, in case it helps heroku

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,5 @@
 require 'authlogic'
+require 'ym4r'
 
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
