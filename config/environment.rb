d4d1eb4750ccf72c8d50e891ba2ba75dd7e7a734
Oren Teich <oren@teich.net>
2009-04-24 16:44:45 -0700

    Added NewRelic RPM back in as gem

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
 
   config.gem "authlogic"
   config.gem "derailed-ziya", :lib => "ziya"
+  config.gem "newrelic_rpm" 
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
