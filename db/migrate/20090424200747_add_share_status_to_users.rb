794b0896e3e531b8df4f1bdb624cf1fd066506f3
Oren Teich <oren@teich.net>
2009-04-24 16:41:22 -0700

    Changed boolena to false

--- db/migrate/20090424200747_add_share_status_to_users.rb
+++ db/migrate/20090424200747_add_share_status_to_users.rb
@@ -1,6 +1,6 @@
 class AddShareStatusToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :shared, :boolean, :default => 0
+    add_column :users, :shared, :boolean, :default => false
   end
 
   def self.down
