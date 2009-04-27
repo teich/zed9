dee324cb8c607092a757ce69bb10f9db5b81d9bb
Oren Teich <oren@teich.net>
2009-04-27 16:35:49 -0700

    Nested shallow routes under user, plus using login vs. id

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resource  :user_session
   map.resource  :dashboard, :only => [:show] 
+  # I think I need to move this to a new controller called account.
   map.resource  :account, :controller => "users"
   map.resources :activities, :only =>[:index]
 
   map.resources :invitations
-#  map.resources :workouts, :path_prefix => ':user_name'  do |workout|
-  map.resources :workouts do |workout|
-
+ 
+  map.resources :users, :only => [:show], :shallow => true do |user|
+    user.resources :workouts do |workout|
       workout.heart_rate_chart 'chart', :controller => 'charts', :action => 'workout_heart_rate' 
       workout.resources :taggings, :only => [:destroy, :create]
+    end
   end
   
   map.signup '/signup/:invitation_token', :controller => 'users', :action => 'new'
