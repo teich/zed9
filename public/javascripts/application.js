513b584577d5531207b34bcb740a83544dd24d0c
oteich <olivia@teich.net>
2009-05-01 14:09:46 -0700

    minor fixes and heart rate big graph point tracking

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -281,6 +281,54 @@ $(document).ready(function () {
 	// });
 	// 
 
+	// -----------------------------
+	// Graph tooltip on hover
+
+  function showTooltip(x, y, contents) ({
+					content: 'foo',
+          position: 'absolute',
+          display: 'none',
+          top: y + 5,
+          left: x + 5,
+          border: '1px solid #fdd',
+          padding: '2px',
+          'background-color': '#fee',
+          opacity: 0.80
+      }).appendTo("body").fadeIn(200);
+
+  var previousPoint = null;
+  $("#fullsize_chart").bind("plothover", function (event, pos, item) {
+      $("#x").text(pos.x.toFixed(2));
+      $("#y").text(pos.y.toFixed(2));
+
+      // if ($("#enableTooltip:checked").length > 0) {
+          if (item) {
+              if (previousPoint != item.datapoint) {
+                  previousPoint = item.datapoint;
+                  
+                  $("#tooltip").remove();
+                  var x = item.datapoint[0].toFixed(2),
+                      y = item.datapoint[1].toFixed(2);
+                  
+                  showTooltip(item.pageX, item.pageY,
+                              item.series.label + " of " + x + " = " + y);
+              }
+          }
+          else {
+              $("#tooltip").remove();
+              previousPoint = null;            
+          }
+      // }
+  });
+
+  $("#fullsize_chart").bind("plotclick", function (event, pos, item) {
+      if (item) {
+          $("#clickdata").text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
+          plot.highlight(item.series, item.datapoint);
+      }
+  });
+
+	// -----------------------------
 
 	$('.stat').qtip({
 			content: '<div class="stat"><p class="comp_this_workout"><span class="value">1:32</span>h for this hike</p> <p class="comp_my_activity"><span class="value">2:08</span>h average for all your hikes</p> <p class="comp_activity"><span class="value">0:55</span>h average for everyone\'s hikes</p></div>',
