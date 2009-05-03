c1fd1c3bd59ce7a8bd767e7a60895d5a6a083445
oteich <olivia@teich.net>
2009-05-03 13:06:06 -0700

    Added newsfeed to marketing page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,6 +3,10 @@ class RootController < ApplicationController
   layout "public"
   
   def index
+
+    # list of 10 most recent public workouts
+    @shared = Workout.find_all_by_shared(true, :limit=>10, :order => "updated_at DESC")
+
     @user_session = UserSession.new
     
     if current_user
