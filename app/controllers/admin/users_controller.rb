e5381a4da3f130ac99db4a66bc5c2212881c6e75
Oren Teich <oren@teich.net>
2009-05-09 16:24:54 -0700

    Fixed up user admin to show more useful information

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,14 @@
 class Admin::UsersController < ApplicationController
-  before_filter :require_admin
-  
-  def index
-    @users = User.find(:all)
-    @invitations = Invitation.find(:all)
-  end
-  
+	before_filter :require_admin
+
+	def index
+		@users = User.find(:all)
+		
+		@invited = Invitation.find(:all, :conditions => ['sent_at > ?', 10.year.ago])
+		
+		@waiting = Invitation.find_all_by_sender_id(nil)
+		
+		
+	end
+
 end
