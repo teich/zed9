fdeff67916bea731b90b3060a0f34b3ff5ce6ad8
Oren Teich <oren@teich.net>
2009-05-09 18:15:50 -0700

    Added batchbox bcc

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -3,7 +3,7 @@ class Mailer < ActionMailer::Base
     subject    'Zed9 Social Fitness Analytics - Alpha Invitation'
     recipients invitation.recipient_email
     from       'oren@zednine.com'
-	bcc			'dropbox@01924470.zed9.highrisehq.com'
+	bcc			['dropbox@01924470.zed9.highrisehq.com', 'batchbox+35165333@zed9.batchbook.com']
     body       :invitation => invitation, :signup_url => signup_url
     invitation.update_attribute(:sent_at, Time.now)
   end
