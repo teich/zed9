a6ca75971b0db7eead3434fe7d96878dd40764bf
Oren Teich <oren@teich.net>
2009-05-12 17:32:12 -0700

    change sender of invite

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
   def invitation(invitation, signup_url)
     subject    'Private Beta Invitation to ZED9 Social Fitness Analytics'
     recipients invitation.recipient_email
-    from       current_user.email
+    from       invitation.sender.email
 	  bcc			['dropbox@01924470.zed9.highrisehq.com', 'batchbox+35165333@zed9.batchbook.com']
     body       :invitation => invitation, :signup_url => signup_url
     invitation.update_attribute(:sent_at, Time.now)
