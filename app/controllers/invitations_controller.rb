5c8ca4a2dfd92ececf8259c7796a87261dd7ffe6
oteich <olivia@teich.net>
2009-05-12 17:15:45 -0700

    Lots of copy cleanup: updates to "private beta" mode and rewritten invite.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,10 +9,10 @@ class InvitationsController < ApplicationController
 		if @invitation.save
 			if logged_in?
 				Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
-				flash[:notice] = "Thank you, invitation sent."
+				flash[:notice] = "Thank you. Invitation sent!"
 				redirect_to dashboard_url
 			else
-				flash[:notice] = "Thank you, we will notify when we are ready."
+				flash[:notice] = "Thank you. We will notify you when we are ready."
 				redirect_to root_url
 			end
 		else
--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,9 +1,9 @@
 class Mailer < ActionMailer::Base
   def invitation(invitation, signup_url)
-    subject    'Zed9 Social Fitness Analytics - Alpha Invitation'
+    subject    'Private Beta Invitation to ZED9 Social Fitness Analytics'
     recipients invitation.recipient_email
-    from       'oren@zednine.com'
-	bcc			['dropbox@01924470.zed9.highrisehq.com', 'batchbox+35165333@zed9.batchbook.com']
+    from       current_user.email
+	  bcc			['dropbox@01924470.zed9.highrisehq.com', 'batchbox+35165333@zed9.batchbook.com']
     body       :invitation => invitation, :signup_url => signup_url
     invitation.update_attribute(:sent_at, Time.now)
   end
