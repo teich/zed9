b3b490ac22ad5ed9e1384e910e238ee8705dc56b
oteich <olivia@teich.net>
2009-05-13 09:42:50 -0700

    No longer cc'ing highrise on invites

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -3,7 +3,7 @@ class Mailer < ActionMailer::Base
     subject    'Invitation to Private Beta for ZED9 Social Fitness Analytics'
     recipients invitation.recipient_email
     from       invitation.sender.email
-	  bcc			['dropbox@01924470.zed9.highrisehq.com', 'batchbox+35165333@zed9.batchbook.com']
+	  bcc			['batchbox+35165333@zed9.batchbook.com']
     body       :invitation => invitation, :signup_url => signup_url
     invitation.update_attribute(:sent_at, Time.now)
   end
