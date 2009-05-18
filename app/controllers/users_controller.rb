0d8efefe714f833c7a5c10ae20598ba91f481f93
oteich <olivia@teich.net>
2009-05-18 13:17:55 -0700

    New user now dumped into upload workout flow.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
-			flash[:notice] = "Account created!"
-			redirect_back_or_default account_url
+			flash[:notice] = "Thanks for setting up your account!<br>Next step: upload a workout."
+			redirect_to new_user_workout_path(@user)
 		else
 			render :action => :new
 		end
