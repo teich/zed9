b2071d6115dabbc220a6d1f6001c2e6db89d1b51
oteich <olivia@teich.net>
2009-05-24 13:33:26 -0700

    Fixed profile view permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-	before_filter :require_no_user, :only => [:new, :show, :create]
+	before_filter :require_no_user, :only => [:new, :create]
 	before_filter :require_user, :only => [:edit, :update]
 
 	def new
