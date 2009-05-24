da9e4931e0f22149e75a521b9d554d29e98df22c
Oren Teich <oren@teich.net>
2009-05-23 19:18:04 -0700

    Fixed sorting - needed an inner span/class to latch onto

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -332,7 +332,13 @@ function workout_page_graphs(data) {
 			});
 
 			// Workouts index table sorting, default to descending on date
-			$("#workouts_index").tablesorter( {sortList: [[2, 1]]} ); 
+			$("#workouts_index").tablesorter({
+				sortList: [[2, 1]],
+				headers: { 
+					0: { sorter: false },
+					9: { sorter: false }
+				}
+			}); 
 
 			// Dismiss flash message
 			$('#flash').click(function() { 
