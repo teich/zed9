f5194690b15d057d8ea89c0a5e77bd7346f2cab0
oteich <olivia@teich.net>
2009-08-08 10:28:31 -0700

    Fixed percent calcs

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -7,9 +7,6 @@ class Gear < ActiveRecord::Base
 	
   acts_as_taggable_on   :tags
 
-  # This is removed so that form can update all fields. May need to correct for tagging workouts with gear.
-  # attr_accessible :name, :tag
-	
 	def distance_used 
     workouts.sum(:distance)
 	end
@@ -19,44 +16,41 @@ class Gear < ActiveRecord::Base
 	end
 	
 	def percent_used_distance
-    if distance_used && distance_max
+    if distance_used && distance_max && distance_max > 0
 	    ((distance_used / distance_max ) * 100 ).round
 	  end
 	end
 
 	def percent_used_hours
-	  if time_used && hours_max
+	  if time_used && hours_max && hours_max > 0
 	    ((time_used / hours_max ) * 100 ).round
 	  end
 	end
 	
 	def percent_remaining
-    if distance_used && distance_max
-      distance_remaining = 100 - ((distance_used / distance_max) * 100 ).round
-      if distance_remaining <= 0
-        distance_remaining = 0
-      end
-    end
 
-    if time_used && hours_max
-      hours_remaining = 100 - ((time_used / hours_max) * 100 ).round
-      if hours_remaining <= 0
-        hours_remaining = 0
-      end
-    end
-    
+    if !distance_max.nil? && distance_max > 0
+      distance_remaining = 100 - percent_used_distance 
+    end  
+
+    if !hours_max.nil? && hours_max > 0
+      hours_remaining = 100 - percent_used_hours 
+    end  
+
     if distance_remaining && hours_remaining
-      if distance_remaining < hours_remaining 
-        return distance_remaining 
-      else hours_remaining < distance_remaining 
-        return hours_remaining 
+      if distance_remaining > hours_remaining
+        return distance_remaining
+      else 
+        return hours_remaining
       end
     elsif distance_remaining
       return distance_remaining
     elsif hours_remaining
       return hours_remaining
-    else return 0
+    else
+      return 0
     end
+    
   end
   
   def percent_remaining_style
