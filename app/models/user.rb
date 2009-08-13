afea23560da54c0e36a0c544441ff05565df8395
oteich <olivia@teich.net>
2009-08-13 13:06:12 -0700

    removed comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
 			:secret_access_key => ENV['S3_SECRET']
 		},
 		:bucket => ENV['S3_BUCKET'],
-		:path => ":class/:id_partition/:basename.:extension",
+		:path => ":class/:id_partition/:basename.:extension"
     # :s3_permissions => "private"
       		
 	before_create :set_invitation_limit
