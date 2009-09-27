7199fb1dddb8e1810ef0175f885c6fc71e3ba418
oren <oren@heroku.com>
2009-09-27 16:42:43 -0700

    Calcs averages

--- app/models/average.rb
+++ app/models/average.rb
@@ -1,15 +1,29 @@
-class Average
+class Average < ActiveRecord::Base
   def self.calc_averages!
-    calc_duration_per_day!
+    duration_per_day = calc_duration_per_day
+    workouts = calc_workouts
+    duration = calc_duration
+    a = Average.create(:duration_per_day => duration_per_day, :workouts => workouts, :duration => duration)
+    a.save
   end
     
-  def self.calc_duration_per_day!
+    
+  def self.calc_workouts
+    User.find(:all).map {|u| u.workouts.processed.size}.aaverage
+  end
+
+  def self.calc_duration
+    Workout.processed.map(&:duration).aaverage
+  end
+  
+  def self.calc_duration_per_day
     collector = []
     users = User.find(:all)
     users.each do |u|
       puts "User #{u.login} has #{u.workouts.processed.size} workouts"
-      if u.workouts.processed.size > 2
+      if u.workouts.processed.size > 0
         days = (u.workouts.processed.last.start_time.to_date - u.workouts.processed.first.start_time.to_date).to_i
+        days += 1 if days == 0
         time = u.workouts.sum(:duration)
         average = time / days
         collector << average
--- db/migrate/20090927230243_create_averages.rb
+++ db/migrate/20090927230243_create_averages.rb
@@ -1,7 +1,9 @@
 class CreateAverages < ActiveRecord::Migration
   def self.up
     create_table :averages do |t|
-
+      t.float :duration_per_day
+      t.float :duration
+      t.integer :workouts
       t.timestamps
     end
   end
