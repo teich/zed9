edabfab960560a17a91b00557b9358b4bd8ffb9b
oren <oren@heroku.com>
2009-09-27 16:21:09 -0700

    Debugging stuff

--- app/models/average.rb
+++ app/models/average.rb
@@ -1,20 +1,21 @@
 class Average
-  def calc_averages!
+  def self.calc_averages!
     calc_duration_per_day!
   end
   
   private
   
-  def calc_duration_per_day!
+  def self.calc_duration_per_day!
     collector = []
     users = User.find(:all)
     users.each do |u|
+      puts "User #{u.login} has #{u.workouts.processed.size} workouts"
       if u.workouts.processed.size > 2
         days = (u.workouts.processed.last.start_time.to_date - u.workouts.processed.first.start_time.to_date).to_i
         time = u.workouts.sum(:duration)
         average = time / days
         collector << average
-        puts "User #{u.login} did #{average}"
+        puts "----> and averaged #{average}"
       end
     end
     duration_per_day = collector.sum.to_f / collector.size
