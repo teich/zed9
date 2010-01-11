38c0bb198b5284f5b420aa837984cea5a2f92c6b
Oren Teich <oren@teich.net>
2010-01-10 17:20:02 -0800

    DRY controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,16 +1,19 @@
 class DataController < ApplicationController
+	before_filter :find_workout
+	
   def sparks
-    workout = Workout.find(params[:workout_id])
-    render :json => workout.comps(params[:q].intern)
+    render :json => @workout.comps(params[:q].intern)
   end
   
   def gis
-    workout = Workout.find(params[:workout_id])
-    render :json => workout.gis
+    render :json => @workout.gis
   end
   
   def full
-    workout = Workout.find(params[:workout_id])
-    render :json => workout.full_data
+    render :json => @workout.full_data
   end
+  
+  def find_workout
+		@workout = Workout.find(params[:workout_id])
+	end
 end
