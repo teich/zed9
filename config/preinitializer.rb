301a79d55cb31608652eae467b2f9046f42cb1c7
Oren Teich <oren@teich.net>
2010-01-16 16:31:28 -0800

    Fixed typo in init

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,12 +1,12 @@
 require "#{File.dirname(__FILE__)}/../vendor/bundler_gems/environment"
-
+ 
 class Rails::Boot
   def run
     load_initializer
     extend_environment
     Rails::Initializer.run(:set_load_path)
   end
-
+ 
   def extend_environment
     Rails::Initializer.class_eval do
       old_load = instance_method(:load_environment)
